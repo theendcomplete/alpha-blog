d59998f5c3d19e5f4ca5dc174530994ee90bedc7
Nikkie Grom <nickonizator@gmail.com>
2018-01-02 08:32:00 -0500

    some new actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,17 @@
 class ArticlesController < ApplicationController 
+	
+	def index
+		@articles = Article.all
+	end
+	
 	def new
 		@article = Article.new
 	end
 	
+	def edit
+			@article = Article.find(params[:id])
+	end
+		
 	def create
 	  	@article = Article.new(article_params)
   		if @article.save
@@ -13,6 +22,15 @@ class ArticlesController < ApplicationController
 		end
   end
 	
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Article was updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end	
 	
   def show
 		@article = Article.find(params[:id])
