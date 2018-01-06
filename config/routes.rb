77f69c873210e32ac4afbb0fdca34a7eb73b59a0
Nikkie Grom <nickonizator@gmail.com>
2018-01-06 11:47:33 -0500

    User registartion

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 root 'pages#home'
 #get 'pages/home', to: 'pages#home'
 get 'about', to: 'pages#about'
+get 'signup', to: 'users#new'
+#post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
