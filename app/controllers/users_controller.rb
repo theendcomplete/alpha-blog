e2153e324699ec2b78d58a7fc3680760ee38523d
Nikkie Grom <nickonizator@gmail.com>
2018-01-13 06:29:13 -0500

    miner added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  def new
+  
+  before_action :set_user, only: [:edit, :update, :show]
+ 
+def new
     @user = User.new
   end
   
@@ -14,11 +17,11 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    
   end
   
   def update
-    @user = User.find(params[:id])
+    
     if @user.update(user_params)
       flash[:success] = "Updated succesfully"
       redirect_to articles_path      
@@ -28,7 +31,7 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
+    
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
   
@@ -41,4 +44,8 @@ class UsersController < ApplicationController
   def user_params
   params.require(:user).permit(:username,:email,:password)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+    end
 end
