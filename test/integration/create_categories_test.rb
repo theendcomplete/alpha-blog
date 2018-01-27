7b88082fc1595e2ec5abe49763e7c532d6a55e39
Nikkie Grom <nickonizator@gmail.com>
2018-01-27 04:22:35 -0500

    category UI

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.create(username: "John admin", email: "Johny@johny.com", password: "password", admin: true)
+  end
+  
+  
   test "get new category form and create new category" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
@@ -12,7 +18,8 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
   
   test "invalid category submission" do
-      get new_category_path
+    sign_in_as(@user, "password")
+    get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
       post  categories_path, category: {name: " "}
