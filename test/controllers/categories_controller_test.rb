47f9117b1004199e4f7d63259f77b8973d1d6c99
Nikkie Grom <nickonizator@gmail.com>
2018-01-27 04:04:28 -0500

    Add admin check to category createion

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionController::TestCase
   
   def setup
    @category = Category.create(name: "sports")
+    @user = User.create(username: "John admin", email: "Johny@johny.com", password: "password", admin: true)
   end
     
   test "shoild get catefories index" do
@@ -12,6 +13,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
   
    test "shoild get new" do
+     session[:user_id] = @user.id
      get :new
     assert_response :success
   end
@@ -21,4 +23,12 @@ class CategoriesControllerTest < ActionController::TestCase
      get(:show, {'id' => @category.id})
     assert_response :success
   end
+  
+  test "should redirect create when admin is not logged in" do
+   assert_no_difference 'Category.count' do 
+     post :create, category: {name: "Sports"}
+     end
+      assert_redirected_to categories_path
+   end
+  
 end
